9fa820c8a900f6de62e785c6dd43441e0e23020b
Dmitriy Glazkov <dglazkov@phoenixit.ru>
2020-10-11 01:32:55 +0300

    refactor styles

--- src/App.js
+++ src/App.js
@@ -138,42 +138,37 @@ function App() {
 
     return (
         <div className="login-page" onMouseMove={onMouseMovePage}>
-            <div className="wrapper">
-                <div className="form">
-                    <form className="login-form" onSubmit={onSubmit}>
-                        <input
-                            type="text"
-                            placeholder="username"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+            <div className="panel login__panel">
+                <form className="panel-login__column login-form" onSubmit={onSubmit}>
+                    <h1 className="title">Вход в систему</h1>
+                    <p className="login__subtitle subtitle">Авторизуйтесь, чтобы использовать все возможности сервиса</p>
+                    <input
+                        type="text"
+                        placeholder="Логин"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Пароль"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <div className="login__submit">
                         <input
                             className="button"
                             type="submit"
-                            value="LOGIN"
+                            value="Войти"
                             /* disabled={!result} */
                         />
-                    </form>
-                </div>
+                    </div>
+                </form>
                 <div
                     style={{
-                        width: WIDTH_CANVAS + 100,
-                        height: HEIGHT_CANVAS + 100
+                        width: WIDTH_CANVAS,
                     }}
-                    className={`wrapper-canvas ${isDrawing ? 'wrapper-canvas_enter' : ''}`}
+                    className={`panel-login__column wrapper-canvas ${isDrawing ? 'wrapper-canvas_enter' : ''}`}
                 >
-                    <p className="canvas__text">
-                        {isPrinted
-                            ? result ? result : "STOP"
-                            : "Рисуйте произвольную фигуру в течении 2-х секунд"
-                        }
-                    </p>
                     <canvas
                         width={WIDTH_CANVAS}
                         height={HEIGHT_CANVAS}
@@ -182,6 +177,32 @@ function App() {
                         id="canvas"
                         className="canvas"
                     />
+                    {!isDrawing && !isPrinted && (
+                        <p className="canvas__status">
+                            <svg x="0px" y="0px" viewBox="0 0 512 512" className="canvas__svg">
+                                <g>
+                                    <g>
+                                        <path d="M256,0C156.595,0,75.726,82.14,75.726,183.099v145.807C75.726,429.865,156.595,512,256,512
+                                            c99.399,0,180.274-81.886,180.274-182.534V183.099C436.274,82.14,355.399,0,256,0z M402.366,329.466
+                                            c0,81.954-65.656,148.627-146.366,148.627c-80.705,0-146.366-66.927-146.366-149.192V183.099
+                                            c0-82.265,65.661-149.192,146.366-149.192c80.711,0,146.366,66.927,146.366,149.192V329.466z"/>
+                                    </g>
+                                </g>
+                                <g>
+                                    <g>
+                                        <path d="M256,140.15c-9.364,0-16.954,7.59-16.954,16.954v59.338c0,9.364,7.59,16.954,16.954,16.954
+                                            c9.364,0,16.954-7.59,16.954-16.954v-59.338C272.954,147.74,265.364,140.15,256,140.15z"/>
+                                    </g>
+                                </g>
+                            </svg>
+                            <span>Нажмите и проведите мышью в этом квадрате. Так мы подтвердим что вы не робот.</span>
+                        </p>
+                    )}
+                    {isPrinted && (
+                        <p className="canvas__status">
+                            {result || "STOP"}
+                        </p>
+                    )}
                     <div className="progressbar">
                         <span style={{ width: `${time / TIMER * 100}%` }} />
                     </div>
--- src/Home.js
+++ src/Home.js
@@ -3,36 +3,61 @@ import React, { useEffect, useState } from 'react';
 let status = '';
 
 function identify(data) {
-    return new Promise((resolve, reject) => {
-        if (data === 123) {
-            // success identify
-            resolve()
-        } else {
-            // fail identify
-            reject()
-        }
+    return fetch('url', {
+        method: 'POST', // or GET
+        headers: {
+            'Authorization': `${localStorage.getItem('token')}` // if need
+        },
+        body: JSON.stringify(data) // some data in json
     })
 }
 
+// here we take response from server in res and if we catch error set status to fail
 identify(123).then(res => status = 'SUCCESS').catch(res => status = 'FAIL')
 
 function Home() {
     const [ state, setState ] = useState();
+    const [ transactionStatus, setTransactionStatus ] = useState('');
 
     useEffect(() => {
         setState(status)
     }, [status])
 
     const onSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        setTransactionStatus('')
+
+        let formData = new FormData();
+        formData.append("AccountNumber", "0000 1111 2222 3333");
+        formData.append("AccountHolder", "John Smith");
+        formData.append("MoneySent", 10);
+
+        fetch('https://morning-tundra-59000.herokuapp.com/transaction', {
+            method: 'POST',
+            headers: {
+                'Authorization': `${localStorage.getItem('token')}`
+            },
+            body: formData
+        }).then(res => {
+            if (res.status === 202) {
+                setTransactionStatus('SUCCESS')
+            } else {
+                setTransactionStatus('FAIL')
+            }
+        }).catch(res => {
+            setTransactionStatus('FAIL')
+        })
     }
 
     return (
         <div className="home">
             <div className="home__panel panel">
-                <p className="status">status: <b>{state}</b></p>
+                <p className="status">auth status: <b>{state}</b></p>
+                <p className="status">transaction status: <b>{transactionStatus}</b></p>
                 <form className="form-home" onSubmit={onSubmit}>
-                    <input type="submit" value="SEND" className="button"/>
+                    <div className="form-home__button">
+                        <input type="submit" value="SEND" className="button"/>
+                    </div>
                 </form>
             </div>
         </div>
